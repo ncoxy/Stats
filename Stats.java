03c23c633894eee0cf6b8fa6711753f296b9dc2c
Cox <truelonghornfan@comcast.net>
2013-09-24 08:15:00 -0600

    Finished all problems

--- Stats.java
+++ Stats.java
@@ -1,14 +1,14 @@
 public class Stats{
 	public static void main(String[] args) {
-		int[] a = {1, 2, 3, 3, 5,};
+		int[] a = {1, 2, 3, 4, 5,};
 		// max(a);
 		// min(a);
 		// mean(a);
 		// median(a);
-		// quartile1(a);
+		quartile1(a);
 		// quartile3(a);
-		mode(a);
-		// standard deviation
+		// mode(a);
+		// standardDeviation(a);
 	}
 	public static int max(int[] a) {
 		int max = a[0];
@@ -59,38 +59,38 @@ public class Stats{
 	public static double quartile1(int[] a){
 		double quartile1 = 0;
 		int length = 0;
-		// double median = median(a);
+		double median = median(a);
 		for (int i=0; i<a.length; i++) {
 			length++;
 
 		}
 		if((length % 2) ==0){
-			quartile1 = a[length - 1] / 4 + 0.5;
+			quartile1 = (double)a[length - 1] / 4 + 0.25;
 		}else{
-			quartile1 = a[length - 1]  / 4;
+			quartile1 = (double)a[length - 1]  / 4;
 		}
 		System.out.println(quartile1);
 		return quartile1;
 	}
-	public static void quartile3(int[] a){
+	public static double quartile3(int[] a){
 		double quartile3 = 0;
 		int length = 0;
-		// double median = median(a);
+		double median = median(a);
 		for (int i=0; i<a.length; i++) {
 			length++;
 
 		}
 		if((length % 2) ==0){
-			quartile3 = a[length - 1] / 4 + 0.5;
+			quartile3 = (double)a[length - 1] * (3/4) + 0.5;
 		}else{
-			quartile3 = a[length - 1]  / 4;
+			quartile3 = (double)a[length - 1]  / (3/4);
 		}
 		System.out.println(quartile3);
 		return quartile3;
 	}
 	public static double mode(int[] a){
-		int maxCount = 0;
 		int counter = 0;
+		int maxCount = 0;
 		int large = 0;
 		for (int i=0; i<a.length; i++) {
 			for(int j=a.length; j<=a[0]; j--){
@@ -107,8 +107,19 @@ public class Stats{
 		System.out.println(maxCount);
 		return maxCount;
 	}
-	public static double standDev(int[] a){
-		
+	public static double standardDeviation(int[] a){
+		double dev = 0;
+		double avg = mean(a);
+		for (int i=0; i<a.length; i++) {
+			dev += (a[i] - avg)*(a[i] - avg);
+		}
+
+		dev = dev / a.length;
+
+		dev = Math.sqrt(dev);
+
+		System.out.println("Standard Deviation: " + dev);
+		return dev;
 	}
 
 
